25b94d49dbfa05f29e95fce09be51fa5ebedae25
Mukui012 <spencermukui@gmail.com>
2023-03-22 02:58:28 -0700

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 1, b = 2, c, i;
+
+	printf("%d, %d", a, b);
+	for (i = 3; i <= 50; i++)
+	{
+		c = a + b;
+		printf(", %d", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
