bfef512f6f6f6a25242c9ab168632f5c329fbb29
Mukui012 <spencermukui@gmail.com>
2023-03-28 01:44:09 -0700

    Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,35 @@
-#include <stdio.h>
+	#include <stdio.h>
 
-/**
- * main - Prints first 50 Fibonacci numbers starting with 1 and 2,
- * separated by a comma and followed by a space
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int a = 1, b = 2, c, i;
+	/**
+	 * main - Entry point
+	 * Prints first 50 Fibonacci numbers, starting with 1 and 2,
+	 * separated by a comma followed by a space.
+	 *
+	 * Return: Always 0.
+	 */
 
-	printf("%d, %d", a, b);
-	for (i = 3; i <= 50; i++)
+	int main(void)
 	{
-		c = a + b;
-		printf(", %d", c);
-		a = b;
-		b = c;
+		int count;
+		unsigned long fib1 = 0, fib2 = 1, sum;
+
+
+		for (count = 0; count < 50; count++)
+		{
+			sum = fib1 + fib2;
+			printf("%lu", sum);
+
+
+			fib1 = fib2;
+			fib2 = sum;
+
+
+			if (count == 49)
+				printf("\n");
+			else
+				printf(", ");
+		}
+
+
+		return (0);
 	}
-	printf("\n");
-	return (0);
-}
