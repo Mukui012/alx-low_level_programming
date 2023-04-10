525a32301720951ec6067372300c18a48433f4bd
Mukui012 <spencermukui@gmail.com>
2023-04-10 13:53:42 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,13 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
 	return (0);
 }
