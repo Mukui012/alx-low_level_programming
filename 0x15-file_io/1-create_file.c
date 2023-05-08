7183d500b51b49b9dce1c932809127aed44b4ad7
Mukui012 <spencermukui@gmail.com>
2023-05-08 10:35:27 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * create_file - creates a file
  * @filename: the file to be created
  * @text_content: text to be written to file
- * Return: 1 success -1 unsuccessful
+ * Return: 1 success -1 error
  */
 
 int create_file(const char *filename, char *text_content)
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	f_write = write(fp, text_content, len);
 
-	if (fp == -1 || w == -1 || w != len)
+	if (fp == -1 || f_write == -1 || f_write != len)
 	{
 		return (-1);
 	}
