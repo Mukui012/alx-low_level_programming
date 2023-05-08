2f0ac47ea2cdb2afefe29c60bb2f3d476abbd2b8
Mukui012 <spencermukui@gmail.com>
2023-05-08 10:27:27 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the file to be created
+ * @text_content: text to be written to file
+ * Return: 1 success -1 unsuccessful
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, fp, f_write;
+	ssize_t len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	len = 0;
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i]; i++)
+		{
+			len++;
+		}
+	}
+
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	f_write = write(fp, text_content, len);
+
+	if (fp == -1 || w == -1 || w != len)
+	{
+		return (-1);
+	}
+	close(fp);
+	return (1);
+}
