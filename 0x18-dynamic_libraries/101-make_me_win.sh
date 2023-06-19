e48284b8bd032c571aa1f7ce428927e04a6b7cca
Mukui012 <spencermukui@gmail.com>
2023-06-19 15:29:06 +0000

    Win the Giga Millions

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qP /tmp/ https://github.com/Mukui012/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigalib.so
-export LD_PRELOAD="/tmp/gigalib.so"
+wget -P /tmp/ https://github.com/Mukui012/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigalib.so
+export LD_PRELOAD=/tmp/gigalib.so
