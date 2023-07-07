ff293183e1c9cda66cb2de89250073ae6525ed6e
Mukui012 <spencermukui@gmail.com>
2023-07-07 18:38:34 +0000

    function that gives you the index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,8 +2,8 @@
 
 /**
  * key_index - function that gives you the index of a key
- * @key - is the key
- * @size - size of the array of the hash table
+ * @key: is the key
+ * @size: size of the array of the hash table
  * Return: index at which the key/value pair should be,
  * stored in the array of the hash table
  */
